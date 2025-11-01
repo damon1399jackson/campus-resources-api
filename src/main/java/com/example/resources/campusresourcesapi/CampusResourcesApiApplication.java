419362eda84564967c3800bb0692b8b222f9d61c
damon1399jackson <123121660+damon1399jackson@users.noreply.github.com>
2025-10-31 22:57:58 -0400

    Initial commit

---
+++ src/main/java/com/example/resources/campusresourcesapi/CampusResourcesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.resources.campusresourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusResourcesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusResourcesApiApplication.class, args);
+    }
+
+}
